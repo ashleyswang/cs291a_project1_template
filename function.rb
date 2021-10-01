3ff2f2d31f5f0a8c8637168bace67f514b1f40ea
Ashley Wang <ashleyswang@ucsb.edu>
2021-09-30 19:17:30 -0700

    start root

--- function.rb
+++ function.rb
@@ -27,7 +27,7 @@ def get_root(event)
   # Check Token
   if event['httpMethod'] != 'GET'
     return response(status: 405)
-  elsif !event.key?('headers') || !event['headers'].key?('Authorization')
+  elsif !event['headers'].key?('Authorization')
     return response(status: 403)
   end
 end
