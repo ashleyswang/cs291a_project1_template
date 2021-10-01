f3d10e87f43541510c8dc0d37cdc09a3c7fa85f2
Ashley Wang <ashleyswang@ucsb.edu>
2021-09-30 17:11:20 -0700

    try adding token stuf

--- function.rb
+++ function.rb
@@ -35,11 +35,13 @@ def post_token(event)
     JSON.parse(event['body'])
 
     payload = {
-      data: { event['body'] },
+      data: event['body'],
       exp: Time.now.to_i + 5,
       nbf: Time.now.to_i + 2
     }
     token = JWT.encode payload, ENV['JWT_SECRET'], 'HS256'
+    body = { token: token }
+    return response(body: body, status: 201)
 
   rescue JSON::ParserError => e  
     response(body: nil, status: 422)
